cf70c335442d34f1d45e8f5f1e508e95e288076c
doohyun ryoo <idfeelme@gmail.com>
2019-11-28 08:57:02 +0900

    add swipe down2

--- src/android/PhotoActivity.java
+++ src/android/PhotoActivity.java
@@ -14,6 +14,7 @@ import android.os.Environment;
 import android.os.StrictMode;
 import android.util.Base64;
 import android.view.View;
+import android.view.MotionEvent;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
